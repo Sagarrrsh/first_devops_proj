0f37aeb17ccc9ad3be26ccea5084a29568988dd5
Sagar S H <158994310+Sagarrrsh@users.noreply.github.com>
2025-08-09 20:57:14 +0530

    new added

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "ubuntu" {
   ami           = "ami-0dba2cb6798deb6d8" # Ubuntu 22.04 LTS in us-east-1 (change if needed)
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   key_name      = "your-ec2-keypair" # Make sure this key pair exists in AWS
 
   tags = {
